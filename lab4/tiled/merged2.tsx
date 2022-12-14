25e116ec72d2e5f0f31ea016e39d6dfef1b8313e
preisy <preus_3@mail.ru>
2022-12-14 13:10:37 +0300

    resources commiting

---
+++ lab4/tiled/merged2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="merged2" tilewidth="32" tileheight="32" tilecount="512" columns="16">
+ <image source="C:/Users/preus/Downloads/merged2.png" width="512" height="1024"/>
+</tileset>
